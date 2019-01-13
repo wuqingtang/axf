7dfdd9f8ac28e02098a27bce53139e15c28627e5
wuqingtang <wu_qing_tang@163.com>
2019-01-13 13:34:05 +0800

    基本框架

---
+++ classroom/clsrm/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from clsrm import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+
+    url(r'^home/$', views.home, name='home'),
+
+    url(r'^market/$', views.market, name='market'),
+
+    url(r'^cart/$', views.cart, name='cart'),
+
+    url(r'^mine/$', views.mine, name='mine'),
+]
---
+++ classroom/clsrm/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'home/home.html')
+
+
+def home(request):
+    return render(request,'home/home.html')
+
+
+def market(request):
+    return render(request, 'market/market.html')
+
+
+def mine(request):
+    return render(request, 'mine/mine.html')
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
